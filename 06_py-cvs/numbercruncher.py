c891706985a655bcba2bc1724c442d34bfae6672
Ivan Yeung <iyeung30@stuy.edu>
2022-09-30 09:27:17 -0400

    Added partner's method and updated header.

--- 06_py-cvs/numbercruncher.py
+++ 06_py-cvs/numbercruncher.py
@@ -8,8 +8,12 @@ DISCO:
 - In order to compare quote character, you need to use \" to signify it
 - .rfind() method finds the last occurence of an input
 - The percentages add up to less than 99.8 calculated in python. Seems to be rounded
+- You can add weights to the function random.choices (but not random.choice)
+- The weights must be a list of either integers or floats
 QCC:
+-Why is csv file displayed differently in github
 -Are the quotations supposed to be removed from occupation title?
+-Is it better to do lines = file.read().spliet('\n') than lines = file.readlines()
 
 HOW THIS SCRIPT WORKS:
 1. populate_dict reads the entire csv file and splits it based on newlines.
@@ -18,9 +22,18 @@ The function goes through the list of job/percent pairs and puts them into dicti
 The method used to populate the dictionary with the pairs depends on whether the occupation contains a \" or not.
 2. Made a list of occupations from the populated dictionary.
 Made a corresponding list of weights from the populated dictionary.
-3. choose_weight randomly chooses a number from 0 to 99.8 and continues to aggregate the weights from index 0 until the
+
+METHOD USING choose_weighted FUNCTION
+3a. choose_weight randomly chooses a number from 0 to 99.8 and continues to aggregate the weights from index 0 until the
 sum surpasses the randomly chosen value. When the randomly chosen value is reached, the index of the last added weight
 is used to determine the occupation that is choosen.
+
+METHOD USING BUILT-IN WEIGHTS in random.choices()
+3b. Each element in the list given in the first parameter of randoms.choices corresponds to the weight of the same index.
+Thus the random is weighted.
+(ex. random.choices([a, b, c], weights = [10, 20, 30]) a has a weight of 10, b has a weight of 20,...)
+
+
 
 """
 import random as rng
@@ -67,16 +80,26 @@ for i in occupations:
 #print(occupations)
 #print(weights)
 
+
 #TESTING WEIGHTED RANDOM
 test = {}
+test2 = {}
 for i in occupations:
     test[i] = 0
-for i in range(1000):
+    test2[i] = 0
+for i in range(100000):
     test[choose_weighted(occupations, weights)] += 1.0
+    test2[rng.choices(list(krewes.keys()), weights = list(krewes.values()))[0]] += 1.0
 for i in occupations:
-    test[i] /= 10
+    test[i] /= 1000
+    test2[i] /= 1000
 
+print(krewes)
+#This is the other method just using the dictionary
+#print(rng.choices(list(krewes.keys()), weights = list(krewes.values()))[0])
+print()
 print(test)
-    
+print()
+print(test2)
     
     
