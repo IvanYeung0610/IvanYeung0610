49fe336854f336e04528b2e79a3715d5425d9233
Ivan Yeung <iyeung30@stuy.edu>
2022-10-02 12:29:08 -0400

    Updated workings functions in 06 and touched up code for 05.

--- 06_py-cvs/numbercruncher.py
+++ 06_py-cvs/numbercruncher.py
@@ -14,7 +14,6 @@ QCC:
 -Why is csv file displayed differently in github
 -Are the quotations supposed to be removed from occupation title?
 -Is it better to do lines = file.read().spliet('\n') than lines = file.readlines()
-
 HOW THIS SCRIPT WORKS:
 1. populate_dict reads the entire csv file and splits it based on newlines.
 It removes the first element(heading) and the last 2(total percentage and extra newline) from the list.
@@ -22,19 +21,14 @@ The function goes through the list of job/percent pairs and puts them into dicti
 The method used to populate the dictionary with the pairs depends on whether the occupation contains a \" or not.
 2. Made a list of occupations from the populated dictionary.
 Made a corresponding list of weights from the populated dictionary.
-
 METHOD USING choose_weighted FUNCTION
 3a. choose_weight randomly chooses a number from 0 to 99.8 and continues to aggregate the weights from index 0 until the
 sum surpasses the randomly chosen value. When the randomly chosen value is reached, the index of the last added weight
 is used to determine the occupation that is choosen.
-
 METHOD USING BUILT-IN WEIGHTS in random.choices()
 3b. Each element in the list given in the first parameter of randoms.choices corresponds to the weight of the same index.
 Thus the random is weighted.
 (ex. random.choices([a, b, c], weights = [10, 20, 30]) a has a weight of 10, b has a weight of 20,...)
-
-
-
 """
 import random as rng
 
@@ -47,18 +41,16 @@ def populate_dict(dictionary, filename):
     total_percentage = data[len(data) - 2]
     data = data[1:len(data) - 2]
     csv_file.close()
-    for i in data:
-        if i[0] == "\"":
-            split_index = i.rfind(",")
-            dictionary[i[1:split_index - 1]] = float(i[split_index + 1:])#[1:split_index - 1] removes " at the beginning the end
+    for line in data:
+        occ = line.rsplit(',', 1) #finds the first comma from the right and splits there
+        if (occ[0] != 'Job Class'): #first one occ[1] can't be converted to a percentage
+            dictionary[occ[0]] = float(occ[1])
         else:
-            job_weight = i.split(",")
-            dictionary[job_weight[0]] = float(job_weight[1])
-            
+            dictionary[occ[0]] = occ[1]
     return total_percentage.split(",")
 
-def choose_weighted(choices, weights): # the choices and weights correspond based on their index in the list
-    random_value = rng.random() * 99.8
+def choose_weighted(choices, weights, total): # the choices and weights correspond based on their index in the list
+    random_value = rng.random() * total
     #print(random_value)
     value = 0
     index = 0
@@ -88,18 +80,18 @@ for i in occupations:
     test[i] = 0
     test2[i] = 0
 for i in range(100000):
-    test[choose_weighted(occupations, weights)] += 1.0
+    test[choose_weighted(occupations, weights, 99.8)] += 1.0
     test2[rng.choices(list(krewes.keys()), weights = list(krewes.values()))[0]] += 1.0
 for i in occupations:
     test[i] /= 1000
     test2[i] /= 1000
 
-print(krewes)
+#print(krewes)
 #This is the other method just using the dictionary
 #print(rng.choices(list(krewes.keys()), weights = list(krewes.values()))[0])
-print()
-print(test)
-print()
-print(test2)
-    
-    
+#print()
+#print(test)
+#print()
+#print(test2)
+print(choose_weighted(occupations,weights, 99.8))
+#print(rng.choices(list(krewes.keys()), weights = list(krewes.values()))[0])
